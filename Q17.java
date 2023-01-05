08f7d8eceb3f0bec5dd3f94ae2180252b35e9949
Sourav Chandra Dey <100287945+SCD-02@users.noreply.github.com>
2023-01-05 09:29:00 +0530

    uploaded code of q17

--- Q17.java
+++ Q17.java
@@ -1,12 +1,24 @@
 
+import java.util.ArrayList;
+
 public class Q17 {
 
-//	public static int[] removeDup(int a[]) {
-//		
-//	}	
-	public static void main(String[] args) {
-		
+  public static ArrayList<Integer> removeDup(int a[]) {
 
-	}
+    ArrayList<Integer> al = new ArrayList<>();
+    al.add(a[0]);
+    for (int i = 1; i < a.length; i++) {
+      if (a[i] != a[i - 1]) {
+        al.add(a[i]);
+      }
+    }
+    return al;
+  }
 
+  public static void main(String[] args) {
+    int a[] = { 1, 2, 2, 3, 3, 3, 4, 4, 4, 5, 6, 7, 7 };
+    ArrayList<Integer> al = removeDup(a);
+    System.out.println(al);
+  }
 }
+
